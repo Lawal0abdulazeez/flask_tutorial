d7dda81e27f9b753a13432ca536a0210b1f1c842
Lawal0abdulazeez <lawalabdulazeezfaruq@gmail.com>
2023-02-18 01:07:58 +0100

    2nd commit

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template,  request
 
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template('index.html')
+    return render_template('index2.html')
+
+
+
+@app.route('/success/<int:score>')
+def success(score):
+    res='pass'
+    return render_template('result.html', result=res)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    res='fail'
+    return render_template('result.html',  result=res)
+
+@app.route('/results/<int:mark>')
+def results(mark):
+    result=''
+    if mark<50:
+        result='fail'
+    else:
+        result='success'
+    return redirect(url_for(result,score=mark))
+
 
 @app.route('/submit', methods=['POST','GET'])
 def submit():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form['science'])
+        maths=float(request.form['maths'])
+        c=float(request.form['c'])
+        data=float(request.form['data'])
+        total_score=(science+maths+c+data)/4
+    res=''
+    if total_score>50:
+        res='success'
+    else:
+        res='fail'
+    return redirect(url_for(res, score=total_score))
+
+
+
     
 
 
